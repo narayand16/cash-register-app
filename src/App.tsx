a6e5b57225082d86e8fe914376dec737e93281ff
NarayanD <narayandeshmukh16@gmail.com>
2023-03-25 17:33:18 +0000

    fix: validate input, create denomination-table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import DenominationsTable from "./DenominationsTable";
 import "./styles.css";
-import "./tailwind.css";
 
 export default function App() {
   const denominations = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
@@ -19,79 +19,65 @@ export default function App() {
     });
     setNoteCounts(newNoteCounts);
   };
-  const inputChangeHandler = (event) => {
-    console.log(
-      "event.target.value",
-      event.target.value,
-      Number(event.target.value),
-      event.target.value === Number(event.target.value),
-      showTable
-    );
-    let inputCash = 0;
-    let inputBill = 0;
-    if (event.target.value < 0) {
+  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (Number(event.target.value) < 0) {
       alert("Invalid amount");
       setShowTable(false);
       return;
     }
     if (event.target.id === "bill-amount") {
       setBillAmt(event.target.value);
-      inputBill = Number(event.target.value);
     }
     if (event.target.id === "cash-given") {
       setCashGiven(event.target.value);
-      inputCash = Number(event.target.value);
-    }
-    if (inputCash >= inputBill) {
-      console.log("here", cashGiven, billAmt);
-      setShowTable(true);
-      calculateChange(inputCash - inputBill);
-    } else {
-      // alert("cash should be equal or more than bill amount");
-      setShowTable(false);
     }
   };
+  const btnClickHandler = () => {
+    if (Number(cashGiven) >= Number(billAmt)) {
+      setShowTable(true);
+      calculateChange(Number(cashGiven) - Number(billAmt));
+    } else {
+      alert("Cash amount must be equal or more than bill amount");
+      setShowTable(false);
+    }
+  }
   return (
-    <div className="App">
-      <h1>Cash Register</h1>
-      <label htmlFor="bill-amount">Bill Amount</label>
-      <input
-        className="block w-full rounded-md border-0"
-        id="bill-amount"
-        value={billAmt}
-        onChange={inputChangeHandler}
-        type="number"
-      />
-      <label htmlFor="cash-given">Cash given</label>
-      <input
-        value={cashGiven}
-        type="number"
-        id="cash-given"
-        onChange={inputChangeHandler}
-      />
+    <div className="App container mx-auto bg-gray-200">
+      <div className="flex justify-center	flex-col items-center px-4 py-7">
+        <h1 className="text-cyan-700 text-lg font-bold">Cash Register App</h1>
+        <p>Enter the bill amount and cash given by the user and
+          know the minimum number of notes to be returned</p>
+        <div className="py-3">
+          <label className="block text-md font-medium leading-6 text-gray-900 mb-2" htmlFor="bill-amount">Bill Amount</label>
+          <input
+            className="block w-80 border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:p-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 md:text-sm sm:leading-6"
+            id="bill-amount"
+            value={billAmt}
+            placeholder="Bill Amount"
+            onChange={inputChangeHandler}
+            type="number"
+          />
+        </div>
+        <div className="py-3">
+          <label className="block text-md font-medium leading-6 text-gray-900 mb-2" htmlFor="cash-given">Cash given</label>
+          <input
+            className="block w-80 border-0 py-1.5 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:p-2 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 md:text-sm sm:leading-6"
+            value={cashGiven}
+            type="number"
+            placeholder="Cash Amount"
+            id="cash-given"
+            onChange={inputChangeHandler}
+          />
+        </div>
+        <button
+          className="my-3 bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
+          onClick={btnClickHandler}>
+          Calculate
+        </button>
 
-      {showTable && (
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th>Denominations</th>
-              {denominations.map((item, index) => {
-                return <td key={index}>{noteCounts[item] > 0 && item}</td>;
-              })}
-            </tr>
-            <tr>
-              <th>No. of notes</th>
-              {denominations.map((item, index) => {
-                return (
-                  <td key={index}>
-                    {noteCounts[item] > 0 && noteCounts[item]}
-                  </td>
-                );
-              })}
-            </tr>
-          </thead>
-        </table>
-      )}
+        {showTable && <DenominationsTable noteCounts={noteCounts} />}
+      </div>
+
     </div>
   );
 }
